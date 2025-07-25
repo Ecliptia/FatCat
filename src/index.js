5f3b2c78a07215b74843a3525ee2b88fe67dff5b
1lucas1apk <dowfriends@gmail.com>
2025-07-25 12:19:06 -0400

    Add `index.js` to handle bot sharding with dynamic shard management and logging.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { ShardingManager } from 'discord.js';
+import logger from "../logger.js";
+import 'dotenv/config'
+
+const manager = new ShardingManager('./src/bot.js', {
+    token: process.env.TOKEN,
+    totalShards: 'auto',
+    shardList: 'auto',
+    mode: 'process',
+    respawn: 'true',
+    timeout: 999999
+});
+
+manager.spawn().then()
+
+manager.on("shardCreate", (shard) => logger.info("Shard " + shard.id + " created"))

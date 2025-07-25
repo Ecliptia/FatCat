0a266ab3b634fa262aaa633298649648dee258c9
1lucas1apk <dowfriends@gmail.com>
2025-07-25 12:19:48 -0400

    Add `walk.js` utility to recursively retrieve `.js` files from directories

---
+++ src/utils/walk.js
@@ -0,0 +1,18 @@
+import { readdir } from "fs/promises";
+import { join, extname } from "path";
+
+export default async function walk(dir) {
+    let files = [];
+
+    const items = await readdir(dir, { withFileTypes: true });
+    for (const item of items) {
+        const res = join(dir, item.name);
+        if (item.isDirectory()) {
+            files = files.concat(await walk(res));
+        } else if (extname(res) === ".js") {
+            files.push(res);
+        }
+    }
+
+    return files;
+}

a93889c0e5fb2f538e2167b03d293856e3bada2e
1lucas1apk <dowfriends@gmail.com>
2025-07-26 13:23:03 -0400

    Update `botinfo` command with enhanced descriptions, localized responses, new images, and updated translations for both English and Brazilian Portuguese locales

--- src/commands/message/info/botinfo.js
+++ src/commands/message/info/botinfo.js
@@ -60,6 +60,8 @@ export default class BotInfoMessage extends Command {
                     translate('botinfo.thanks', { authorTag: message.author.id }) + '\n\n' +
                     contributorsText
                 )
+                .setThumbnail("https://media.discordapp.net/attachments/1151684866971275325/1398692603691798732/36_Sem_Titulo_20250726124442.png?ex=6886497f&is=6884f7ff&hm=7f6ba32c7e34418d32b47483525ce7275f067f013f1992db0cb82c0ad50fce16&=&format=webp&quality=lossless&width=749&height=810")
+                .setImage("https://media.discordapp.net/attachments/1151684866971275325/1398713965273550980/29_Sem_Titulo_20250726140947.png?ex=68865d64&is=68850be4&hm=3e1f96d0acd617a7af48adb3ee8a17cfa03411ace6a1d1b94bc63d01c2e33ef5&=&format=webp&quality=lossless&width=1540&height=589")
                 .setFooter({
                     text: translate('botinfo.footer', { authorTag: message.author.tag }),
                     iconURL: message.author.avatarURL()

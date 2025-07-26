10d97af7f48c9ec63046b8c28e1a4687e6d37516
1lucas1apk <dowfriends@gmail.com>
2025-07-25 21:26:25 -0400

    Update `events.js` to improve event loading with error handling and logging

--- src/handlers/events.js
+++ src/handlers/events.js
@@ -1,16 +1,27 @@
 import walk from "../utils/walk.js";
 import { dirname, basename } from "path";
 import { fileURLToPath, pathToFileURL } from "url";
+import logger from "../../logger.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default async function loadEvents(client) {
-    const files = await walk(`${__dirname}/../events/client`);
+    const files = await walk(`${__dirname}/../events`);
+    logger.debug('[EventManager] Loading events...');
 
     for (const file of files) {
-        const event = (await import(pathToFileURL(file).href)).default;
-        const name = basename(file, ".js");
+        try {
+            const event = (await import(pathToFileURL(file).href)).default;
+            const name = basename(file, ".js");
 
-        client.on(name, (...args) => event(client, ...args));
+            if (event.once) {
+                client.once(name, (...args) => event.execute(client, ...args));
+            } else {
+                client.on(name, (...args) => event.execute(client, ...args));
+            }
+            logger.debug(`[EventManager] Loaded event: ${name}`);
+        } catch (error) {
+            logger.error(`[EventManager] Failed to load event from file ${file}`, error);
+        }
     }
 }

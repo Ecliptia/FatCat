9ba39c90e3669ebd48e6109603858610ea7e68c2
1lucas1apk <dowfriends@gmail.com>
2025-07-26 17:33:22 -0400

    Enhance `ping` command to include database latency, improve uptime formatting, and update thumbnail reference for better clarity and accuracy.

--- src/commands/message/info/ping.js
+++ src/commands/message/info/ping.js
@@ -2,6 +2,9 @@ import Command from '../../../structures/Command.js';
 import logger from '../../../../logger.js';
 import { EmbedBuilder } from 'discord.js';
 import config from '../../../../config.js';
+import formatUptime from '../../../utils/formatUptime.js';
+import { database, ref, get } from '../../../database/firebase.js';
+import { child } from 'firebase/database';
 
 export default class PingMessage extends Command {
     constructor() {
@@ -22,10 +25,18 @@ export default class PingMessage extends Command {
             const apiLatency = Math.round(message.client.ws.ping);
 
             const uptime = process.uptime();
-            const days = Math.floor(uptime / 86400);
-            const hours = Math.floor((uptime % 86400) / 3600);
-            const minutes = Math.floor((uptime % 3600) / 60);
-            const seconds = Math.floor(uptime % 60);
+            const formattedUptime = formatUptime(uptime);
+
+            let databaseLatency = 'N/A';
+            try {
+                const dbStartTime = process.hrtime.bigint();
+                await get(child(ref(database), 'guildSettings/test'));
+                const dbEndTime = process.hrtime.bigint();
+                databaseLatency = Math.round(Number(dbEndTime - dbStartTime) / 1_000_000);
+            } catch (dbError) {
+                logger.error(`[Command:ping-message] Falha ao obter latência do banco de dados:`, dbError);
+                databaseLatency = translate('ping.database_disconnected');
+            }
 
             const embed = new EmbedBuilder()
                 .setColor(config.embedColor)
@@ -33,10 +44,10 @@ export default class PingMessage extends Command {
                 .setDescription(
                     `${translate('ping.api_latency', { apiLatency })}\n` +
                     `${translate('ping.bot_latency', { latency })}\n` +
-                    `${translate('ping.database_latency')}\n` +
-                    `${translate('ping.field_uptime')}: \`${days}d ${hours}h ${minutes}m ${seconds}s\``
+                    `${translate('ping.database_latency', { databaseLatency })}\n` +
+                    `${translate('ping.field_uptime')}: \`${formattedUptime}\``
                 )
-                .setThumbnail("https://cdn3.emoji.gg/emojis/51721-gleep-glorp-cat.png")
+                .setThumbnail(config.images.pingThumbnail)
                 .setFooter({
                     text: translate('ping.requested_by', { authorTag: message.author.tag }),
                     iconURL: message.author.avatarURL()
@@ -53,7 +64,7 @@ export default class PingMessage extends Command {
                 embeds: [embed]
             });
 
-            logger.debug(`[Command:ping-message] Respondeu para ${message.author.tag} com latência: ${latency}ms, Latência API: ${apiLatency}ms`);
+            logger.debug(`[Command:ping-message] Respondeu para ${message.author.tag} com latência: ${latency}ms, Latência API: ${apiLatency}ms, Latência DB: ${databaseLatency}ms`);
         } catch (error) {
             logger.error(`[Command:ping-message] Falha ao executar comando para ${message.author.tag}`, error);
             await message.reply(

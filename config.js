7804b3a302dc3ac58e26beea383ec279c75b982c
1lucas1apk <dowfriends@gmail.com>
2025-07-26 17:32:45 -0400

    Add flag emojis, image URLs, and support server link to `config.js` for enhanced localization and resource access

--- config.js
+++ config.js
@@ -3,4 +3,14 @@ export default {
         level: 'debug'
     },
     embedColor: '#2F78FF',
+    flagEmojis: {
+        'pt-BR': '🇧🇷',
+        'en-US': '🇺🇸'
+    },
+    images: {
+        botThumbnail: "https://media.discordapp.net/attachments/1151684866971275325/1398692603691798732/36_Sem_Titulo_20250726124442.png?ex=6886497f&is=6884f7ff&hm=7f6ba32c7e34418d32b47483525ce7275f067f013f1992db0cb82c0ad50fce16&=&format=webp&quality=lossless&width=749&height=810",
+        botImage: "https://media.discordapp.net/attachments/1151684866971275325/1398713965273550980/29_Sem_Titulo_20250726140947.png?ex=68865d64&is=68850be4&hm=3e1f96d0acd617a7af48adb3ee8a17cfa03411ace6a1d1b94bc63d01c2e33ef5&=&format=webp&quality=lossless&width=1540&height=589",
+        pingThumbnail: "https://cdn3.emoji.gg/emojis/51721-gleep-glorp-cat.png"
+    },
+    supportServerLink: "https://discord.gg/q8HzGuHuDY"
 }

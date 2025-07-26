52df8dde330a17451855ed6700c05a66320cc838
1lucas1apk <dowfriends@gmail.com>
2025-07-25 21:32:31 -0400

    Improve `messageCommands.js` to enhance message command loading with error handling, logging, and use of `Collection`.

--- src/handlers/messageCommands.js
+++ src/handlers/messageCommands.js
@@ -1,16 +1,32 @@
+import { Collection } from 'discord.js';
 import walk from "../utils/walk.js";
 import { dirname } from "path";
 import { fileURLToPath, pathToFileURL } from "url";
+import logger from "../../logger.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default async function loadMessageCommands(client) {
-    const files = await walk(`${__dirname}/../commands/message`);
+    client.commands = new Collection();
+    logger.debug('[ComandosMensagem] Iniciando o carregamento dos comandos de mensagem...');
 
-    for (const file of files) {
-        const command = (await import(pathToFileURL(file).href)).default;
-        if (!command?.name) continue;
+    const arquivos = await walk(`${__dirname}/../commands/message`);
 
-        client.commands.set(command.name, command);
+    for (const arquivo of arquivos) {
+        try {
+            const ClasseComando = (await import(pathToFileURL(arquivo).href)).default;
+            const comando = new ClasseComando();
+
+            if (!comando.slash) {
+                client.commands.set(comando.name, comando);
+                logger.debug(`[ComandosMensagem] Comando de mensagem carregado: ${comando.name}`);
+            } else {
+                logger.warn(`[ComandosMensagem] Ignorando comando slash encontrado na pasta de comandos de mensagem: ${comando.name}`);
+            }
+        } catch (erro) {
+            logger.error(`[ComandosMensagem] Falha ao carregar o comando de mensagem do arquivo ${arquivo}`, erro);
+        }
     }
+
+    logger.debug('[ComandosMensagem] Carregamento dos comandos de mensagem concluído.');
 }
